4dbb631bfe15c6aeca6e3ae5729da479a8136103
carlosnovo <charlynovic@gmail.com>
2015-03-10 19:59:13 +0100

    Mi programa

---
+++ Seminario0.py
@@ -0,0 +1,5 @@
+beers = 99
+while beers >0:
+    print beers," bottles of beer on the wall, ",beers," bottles of beer."
+    beers = beers-1
+    print "Take one down, pass it around, ", beers," bottles of beer on the wall."
